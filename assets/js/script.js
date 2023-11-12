0c0d3a7318e5b5be46225a1a8e8684ab238a70d5
matus42 <134312272+matus42@users.noreply.github.com>
2023-11-12 19:10:15 +0000

    Making request using GET method

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const API_KEY = "uG7Yr7gW9iWAilPN34R2x-ZQOQg";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error)
+    }
+}
+
+function displayStatus(data) {
+    
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until:</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
